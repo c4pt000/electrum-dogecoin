71e30a9c17125072401e98fa261a908a4c5617f1
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-07-08 22:45:13 -0400

    Create ubuntu-install.sh

---
+++ ubuntu-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ apt-get update && \
+ apt install git nano sudo wget python3 python3-pip python3-dev libsecp256k1-0 python3-pyqt5 -y
+ cd /opt/
+ git clone https://github.com/c4pt000/electrum-radiocoin
+ cd electrum-radiocoin/
+ python3 -m pip install cryptography
+ python3 -m pip install . && ./run_electrum_radc 
