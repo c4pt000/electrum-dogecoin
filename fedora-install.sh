50dd815d85e7627f092eb47eada91685cb57c14f
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-07-10 07:26:56 -0400

    Update fedora-install.sh

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
-echo "Fedora 34 install for electrum-radiocoin"
+echo "Fedora 34 install for electrum-dogecoin"
 yum install python3-pip git libsecp256k1-devel.x86_64 libsecp256k1 mesa* \
 nano qrencode-devel qt5-qtbase-devel.x86_64 qt-devel qt4-devel wget -y
 
 	cd /opt/
 
-	git clone https://github.com/c4pt000/electrum-radiocoin
-	cd electrum-radiocoin
+	git clone https://github.com/c4pt000/electrum-dogecoin
+	cd electrum-dogecoin
 
 	python3 -m pip install cryptography PyQt5
 	python3 -m pip install .
-	./run_electrum_radc 
+	./run_electrum_doge
