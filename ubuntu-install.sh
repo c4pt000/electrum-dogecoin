6122678042abb594ad2ba5dfb264c80061ea77e8
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-07-30 05:28:54 -0400

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  apt-get update && \
- apt install git nano sudo wget python3 python3-pip python3-dev libsecp256k1-0 python3-pyqt5 zbar -y
+ apt install git nano sudo wget python3 python3-pip python3-dev libsecp256k1-0 python3-pyqt5 google*fonts zbar -y
  cd /opt/
  git clone https://github.com/c4pt000/electrum-dogecoin
  cd electrum-dogecoin/
