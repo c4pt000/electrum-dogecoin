65b61bef4631769e196d4fc324a34b98341dcae2
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-07-30 05:29:10 -0400

    Update fedora-install.sh

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 echo "Fedora 34 install for electrum-dogecoin"
-yum install python3-pip git libsecp256k1-devel.x86_64 libsecp256k1 mesa* zbar \
+yum install python3-pip git libsecp256k1-devel.x86_64 libsecp256k1 mesa* google*fonts zbar \
 nano qrencode-devel qt5-qtbase-devel.x86_64 qt-devel qt4-devel wget -y
 
 	cd /opt/
